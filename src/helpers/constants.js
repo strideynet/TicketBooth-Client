55fbe3cadd62231a6aff76279a50fc9a37c72e69
Noah Stride <noah@noahstride.co.uk>
2019-07-14 16:48:18 +0100

    Participant modifications/some refactoring

---
+++ src/helpers/constants.js
@@ -0,0 +1,14 @@
+export const genderOptions = [
+  {
+    text: 'Male',
+    value: 'male'
+  },
+  {
+    text: 'Female',
+    value: 'female'
+  },
+  {
+    text: 'Other',
+    value: 'other'
+  }
+]
